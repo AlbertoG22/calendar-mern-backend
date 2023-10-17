9e361a262de8fd2635c5439ad36217d20c5a253a
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-10-16 18:41:13 -0600

    adding express validator for login and createUsers

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,20 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 // esto del 'express.response' es solo para tener el autocompletado dentro de la función
 const crearUsuario = (req, res = response) => {
     const { name, email, password } = req.body;
 
-    if ( name.length < 5 ) {
+    // manejo de errores
+    const errors = validationResult( req );
+    if ( !errors.isEmpty() ) {
         return res.status(400).json({
             ok: false,
-            msg: 'El nombre debe ser de 5 letras'
+            errors: errors.mapped()
         });
     }
 
-    res.json({
+    res.status(201).json({
         ok: true,
         msg: 'registro',
         name,
@@ -23,7 +26,15 @@ const crearUsuario = (req, res = response) => {
 const loginUsuario = (req, res = response) => {
     const { email, password } = req.body;
 
-    res.json({
+    const errors = validationResult(req);
+    if ( !errors.isEmpty() ) {
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        })
+    }
+
+    res.status(201).json({
         ok: true,
         msg: 'login',
         email,
