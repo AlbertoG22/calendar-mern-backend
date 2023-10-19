c793ac5b5097208da52d19e2f67db1ff8fe4e5b2
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-10-18 19:03:18 -0600

    generate JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const { response } = require('express');
 const bcrypt = require('bcryptjs');
 const Usuario = require('../models/User');
+const { generarJWT } = require('../helpers/jwt');
 
 const crearUsuario = async(req, res = response) => {
     const { email, password } = req.body;
@@ -25,12 +26,13 @@ const crearUsuario = async(req, res = response) => {
         await usuario.save(); // <-- Promise
 
         // generar JWT
-        
+        const token = await generarJWT( usuario.id, usuario.name );
     
         res.status(201).json({
             ok: true,
             uid: usuario.id,
-            name: usuario.name
+            name: usuario.name,
+            token
         });
     } catch (error) {
         console.log(error);
@@ -39,7 +41,6 @@ const crearUsuario = async(req, res = response) => {
             msg: 'Por favor hable con el admin.'
         });
     }
-
 }
 
 const loginUsuario = async(req, res = response) => {
@@ -67,11 +68,13 @@ const loginUsuario = async(req, res = response) => {
         }
 
         // generar JWT
+        const token = await generarJWT( usuario.id, usuario.name );
 
         res.json({
             ok: true,
             uid: usuario.id,
-            name: usuario.name
+            name: usuario.name,
+            token
         });
 
     } catch (error) {
