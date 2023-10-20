98988f5cccacab802fc32037736daa56c8985fc7
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-10-19 18:36:38 -0600

    remove event from db

--- controllers/events.js
+++ controllers/events.js
@@ -33,7 +33,6 @@ const crearEvento = async(req, res = response) => {
 };
 
 const actualizarEvento = async(req, res = response) => {
-
     const eventoId = req.params.id;
     const uid = req.uid;
 
@@ -41,7 +40,7 @@ const actualizarEvento = async(req, res = response) => {
         const evento = await Evento.findById(eventoId);
 
         if ( !evento ) {
-            res.status(404).json({
+            return res.status(404).json({
                 ok: false,
                 msg: 'Evento no existe por ese id'
             });
@@ -49,7 +48,7 @@ const actualizarEvento = async(req, res = response) => {
 
         // revisar que el usuario que creó el evento es el mismo que lo quiere actualizar
         if ( evento.user.toString() !== uid ) {
-            res.status(401).json({
+            return res.status(401).json({
                 ok: false,
                 msg: 'No tiene privilegio de editar este evento'
             });
@@ -76,12 +75,41 @@ const actualizarEvento = async(req, res = response) => {
     }
 };
 
-const eliminarEvento = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'eliminarEvento',
-        // id: req.params.id
-    });
+const eliminarEvento = async(req, res = response) => {
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        const evento = await Evento.findById(eventoId);
+
+        if ( !evento ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe por ese id'
+            });
+        }
+
+        // revisar que el usuario que creó el evento es el mismo que lo quiere actualizar
+        if ( evento.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegio de eliminar este evento'
+            });
+        }
+
+        await Evento.findByIdAndDelete( eventoId );
+
+        res.json({
+            ok: true,
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
 };
 
 module.exports = {
