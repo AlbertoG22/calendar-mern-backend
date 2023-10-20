03a42005336705715dacbe1556c465dc4de464a0
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-10-19 18:23:13 -0600

    update event

--- controllers/events.js
+++ controllers/events.js
@@ -32,12 +32,48 @@ const crearEvento = async(req, res = response) => {
     }
 };
 
-const actualizarEvento = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'actualizarEvento',
-        // id: req.params.id
-    });
+const actualizarEvento = async(req, res = response) => {
+
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        const evento = await Evento.findById(eventoId);
+
+        if ( !evento ) {
+            res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe por ese id'
+            });
+        }
+
+        // revisar que el usuario que creó el evento es el mismo que lo quiere actualizar
+        if ( evento.user.toString() !== uid ) {
+            res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegio de editar este evento'
+            });
+        }
+
+        const nuevoEvento = {
+            ...req.body,
+            user: uid
+        };
+
+        const eventoActualizado = await Evento.findByIdAndUpdate( eventoId, nuevoEvento, { new: true } );
+
+        res.json({
+            ok: true,
+            evento: eventoActualizado
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
 };
 
 const eliminarEvento = (req, res = response) => {
